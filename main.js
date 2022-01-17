225b851f9f3dbc868805014cccd480d6e7db8df4
Connor Henderson <connorhenderson20@gmail.com>
2022-01-17 16:31:47 -0500

    Update bot folder to repository

---
+++ main.js
@@ -0,0 +1,16 @@
+// Grabbing discxord libraries
+const Discord = require('discord.js');
+
+// creating discord bot client
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+
+
+
+// LAST LINE
+client.login('OTMyNzE5NTY3MzcxOTI3NjMy.YeXEyQ.fnGIXnAQ0GPdLmfgfLJNr3T-AGo');
